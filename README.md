a53007e9a88f6f9ea5caa2a91df2ce61936477e6
Ckat <ckat@teknik.io>
2017-07-19 19:58:30 +0200

    added files

---
+++ README.md
@@ -0,0 +1,11 @@
+# xresources2minttyrc
+convert .Xresources colorschemes to minttyrc colorschemes
+
+usage:
+
+    xresources2minttyrc.sh .Xresources >> ~/.minttyrc
+    
+as a test don't output to file but just watch what it outputs and if that makes sense(no errors or colors not being found)
+
+
+it's assumed you have them in a normal format, this means no outcommented colors or things along those lines, other stuff in the .Xresources file shouldn't matter as its ignored. a good site to get these compatible formatted .Xresources files is [terminal.sexy](https://terminal.sexy)
---
+++ xresources2minttyrc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# script to parse Xresources colorschemes to minttyrc format
+# Ckat 19/7/2017
+
+function getCol {
+    awk '/'"$1"'/{ sub(/#/,""); print $2 }' "$FILE"
+}
+
+if [[ -z "$1" ]]; then
+    echo "please specify an Xresources file"
+    exit 1
+fi
+FILE="$1"
+
+col=$(getCol "foreground")
+printf "ForegroundColour=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "background")
+printf "BackgroundColour=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "cursorColor")
+printf "CursorColour=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color0")
+printf "Black=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color8")
+printf "BoldBlack=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color1")
+printf "Red=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color9")
+printf "BoldRed=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color2")
+printf "Green=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color10")
+printf "BoldGreen=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color3")
+printf "Yellow=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color11")
+printf "BoldYellow=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color4")
+printf "Blue=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color12")
+printf "BoldBlue=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color5")
+printf "Magenta=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color13")
+printf "BoldMagenta=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color6")
+printf "Cyan=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color14")
+printf "BoldCyan=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color7")
+printf "White=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
+col=$(getCol "color15")
+printf "BoldWhite=%d,%d,%d\n" 0x${col:0:2} 0x${col:2:2} 0x${col:4:2}
